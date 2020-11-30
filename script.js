6f7fe8fe338d235f8f6be92d7492119362cca3c7
Antonne Turner <antonneturner@gmail.com>
2020-11-30 13:31:46 -0500

    add columns and rows to match display of activity, create variable for api key, create search button function in js

---
+++ script.js
@@ -0,0 +1,20 @@
+var apiKey="64126c4733c820feb7b19413d2391072"
+$("#search").on ("click", function(){
+    displayWeather()
+})
+function displayWeather(){
+
+    var cityName=$("#cityName").val()
+    $.ajax({
+        url:"https://api.openweathermap.org/data/2.5/weather?q="+cityName +"&units=imperial&appid="+apiKey,
+        method:"GET"
+    })
+    .then(function(results){
+        console.log(results)
+
+        var h1=$("<h1>").html("temp:" + results.main.temp)
+        $(".jumbotron").append(h1)
+    })
+
+        var h2=$("<h2>") .html("humidity:" + results.main.humidity)
+}
